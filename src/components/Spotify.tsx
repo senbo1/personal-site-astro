a29404bd13bd71c196b8106d2ca83593b59853c3
senbo1 <harshbhadu86@gmail.com>
2023-12-17 01:16:48 +0530

    added audioplayback

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -1,9 +1,10 @@
 import { useState, useCallback, useEffect, useRef, type FC } from 'react';
 import type { Song } from '../lib/types';
-import SpotifyIcon from './Icons/SpotifyIcon';
+import { Icons } from './Icons/ReactIcons';
 
 const Spotify: FC = () => {
   const [song, setSong] = useState<Song>();
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null); 
   const progress = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -27,6 +28,23 @@ const Spotify: FC = () => {
     return () => clearInterval(interval);
   }, []);
 
+  const downloadAndPlayPause = useCallback(() => {
+    if (audio && !audio.paused) {
+      audio.pause();
+    } else if (audio && audio.paused) {
+      audio.play();
+    } else if (!audio && song) {
+      fetch(song.previewUrl)
+        .then((res) => res.blob())
+        .then((blob) => {
+          const objectURL = URL.createObjectURL(blob);
+          const newAudio = new Audio(objectURL);
+          setAudio(newAudio)
+          newAudio.play();
+        });
+    }
+  }, [song?.previewUrl, audio]);
+
   const formatTime = useCallback((seconds: number): string => {
     const minutes = Math.floor(seconds / 60)
       .toString()
@@ -39,11 +57,11 @@ const Spotify: FC = () => {
     return (
       <section className="flex flex-col gap-1 my-2">
         <h2 className="font-bold flex items-center gap-2 underline underline-offset-4 decoration-neutral-500 hover-transition">
-          <SpotifyIcon className='h-6 ml-1'/>
+          <Icons.SpotifyIcon className="h-6 ml-1" />
           Nothing Playing Right now!
         </h2>
         <div className="flex justify-center">
-          <img src="/wall.jpg" alt="Chill Clouds Image" className='w-full'/>
+          <img src="/wall.jpg" alt="Chill Clouds Image" className="w-full" />
         </div>
       </section>
     );
@@ -52,7 +70,7 @@ const Spotify: FC = () => {
   return (
     <section className="flex flex-col gap-2 my-3">
       <h2 className="font-bold flex items-center gap-2 underline underline-offset-4 decoration-neutral-500 hover-transition">
-        <SpotifyIcon className='h-6 ml-1'/>
+        <Icons.SpotifyIcon className="h-6 ml-1" />
         Currently {song.isCurrentlyPlaying ? 'Playing' : 'Paused'}
       </h2>
       <div className="flex">
@@ -65,19 +83,27 @@ const Spotify: FC = () => {
         />
         <div className="flex flex-col justify-between w-full">
           <div className="flex">
-            <a
-              className="font-bold hover-transition mx-auto truncate"
-              href={song.externalUrl}
-              target="_blank"
-            >
-              {song.title} - {song.artist.name}
-            </a>
+            <div className="mx-auto flex flex-col">
+              <a
+                className="font-bold hover-transition mx-auto truncate"
+                href={song.externalUrl}
+                target="_blank"
+              >
+                {song.title} - {song.artist.name}
+              </a>
+              <button
+                className="mx-auto hover-transition"
+                onClick={downloadAndPlayPause}
+              >
+                Play/Pause
+              </button>
+            </div>
             <p className="font-mono">
               {formatTime(Math.floor(song.progress_ms / 1000))}
             </p>
           </div>
-          <div className="flex justify-center">
-            <div className="w-3/4 h-1 bg-neutral-600 rounded-full">
+          <div className="flex justify-center mx-auto w-3/4">
+            <div className="w-full h-1 bg-neutral-600 rounded-full">
               <div
                 className="h-full bg-neutral-50 rounded-full"
                 ref={progress}
